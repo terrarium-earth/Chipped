9dd793a3a99828256e93a1fa801499d02657fc67
Ashley Wright <ahmedtrg981106@gmail.com>
2021-06-25 23:26:38 +0300

    Suppliers.memoize is nice

--- src/main/java/com/grimbo/chipped/block/ChippedWorkbench.java
+++ src/main/java/com/grimbo/chipped/block/ChippedWorkbench.java
@@ -1,5 +1,6 @@
 package com.grimbo.chipped.block;
 
+import com.google.common.base.Suppliers;
 import com.grimbo.chipped.menus.ChippedMenu;
 import com.grimbo.chipped.recipe.ChippedRecipe;
 import net.fabricmc.api.EnvType;
@@ -9,7 +10,6 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.util.LazyLoadedValue;
 import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -19,12 +19,11 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
@@ -36,8 +35,10 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.function.Supplier;
+
 public class ChippedWorkbench extends Block {
-    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
 	protected static final VoxelShape WORKBENCH_NORTH_SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
 	protected static final VoxelShape WORKBENCH_EAST_SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
@@ -46,15 +47,15 @@ public class ChippedWorkbench extends Block {
 
 	public static final EnumProperty<WorkbenchModelType> MODEL_TYPE = EnumProperty.create("model", WorkbenchModelType.class);
 
-    private final MenuType<ChippedMenu> menuType;
-    private final RecipeType<ChippedRecipe> recipeType;
-    private final LazyLoadedValue<Component> containerName;
+	private final MenuType<ChippedMenu> menuType;
+	private final RecipeType<ChippedRecipe> recipeType;
+	private final Supplier<Component> containerName;
 
 	public ChippedWorkbench(MenuType<ChippedMenu> menuType, RecipeType<ChippedRecipe> recipeType, Properties properties) {
 		super(properties);
-        this.menuType = menuType;
-        this.recipeType = recipeType;
-		containerName = new LazyLoadedValue<>(() -> new TranslatableComponent("container.chipped." + Registry.BLOCK.getKey(ChippedWorkbench.this).getPath()));
+		this.menuType = menuType;
+		this.recipeType = recipeType;
+		containerName = Suppliers.memoize(() -> new TranslatableComponent("container.chipped." + Registry.BLOCK.getKey(ChippedWorkbench.this).getPath()));
 		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(MODEL_TYPE, WorkbenchModelType.MAIN));
 	}
 
@@ -75,11 +76,11 @@ public class ChippedWorkbench extends Block {
 	@Nullable
 	@Override
 	public MenuProvider getMenuProvider(BlockState state, Level worldIn, BlockPos pos) {
-        return new SimpleMenuProvider(
-                (id, inventory, player) -> new ChippedMenu(id, inventory, menuType, recipeType, ContainerLevelAccess.create(worldIn, pos), this),
-                containerName.get()
-        );
-    }
+		return new SimpleMenuProvider(
+				(id, inventory, player) -> new ChippedMenu(id, inventory, menuType, recipeType, ContainerLevelAccess.create(worldIn, pos), this),
+				containerName.get()
+		);
+	}
 
 	@Override
 	public void playerWillDestroy(Level worldIn, BlockPos pos, BlockState state, Player player) {
@@ -119,14 +120,14 @@ public class ChippedWorkbench extends Block {
 	@Override
 	public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
 		switch (blockState.getValue(FACING)) {
-		case NORTH:
-			return WORKBENCH_NORTH_SHAPE;
-		case SOUTH:
-			return WORKBENCH_SOUTH_SHAPE;
-		case WEST:
-			return WORKBENCH_WEST_SHAPE;
-		default:
-			return WORKBENCH_EAST_SHAPE;
+			case NORTH:
+				return WORKBENCH_NORTH_SHAPE;
+			case SOUTH:
+				return WORKBENCH_SOUTH_SHAPE;
+			case WEST:
+				return WORKBENCH_WEST_SHAPE;
+			default:
+				return WORKBENCH_EAST_SHAPE;
 		}
 	}
 
@@ -162,22 +163,17 @@ public class ChippedWorkbench extends Block {
 		return 1;
 	}
 
-	@FunctionalInterface
-	public interface ContainerFactory {
-        ChippedMenu create(int windowId, Inventory inventory, ContainerLevelAccess access);
-    }
+	public enum WorkbenchModelType implements StringRepresentable {
+		MAIN, SIDE;
 
-    public enum WorkbenchModelType implements StringRepresentable {
-        MAIN, SIDE;
+		@Override
+		public String getSerializedName() {
+			return name().toLowerCase();
+		}
 
-        @Override
-        public String getSerializedName() {
-            return name().toLowerCase();
-        }
-
-        @Override
-        public String toString() {
-            return getSerializedName();
-        }
-    }
+		@Override
+		public String toString() {
+			return getSerializedName();
+		}
+	}
 }
