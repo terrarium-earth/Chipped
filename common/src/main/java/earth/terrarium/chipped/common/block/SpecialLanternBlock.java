cd40a26600a976642a5228bdc32dfa00561bc6f2
Alex Nijjar <alexander.nijjar@icloud.com>
2023-02-25 19:53:12 -0600

    fixed #173

--- common/src/main/java/earth/terrarium/chipped/common/block/SpecialLanternBlock.java
+++ common/src/main/java/earth/terrarium/chipped/common/block/SpecialLanternBlock.java
@@ -5,19 +5,26 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
+@SuppressWarnings("deprecation")
 public class SpecialLanternBlock extends Block implements SimpleWaterloggedBlock {
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
     private final VoxelShape toEast;
     private final VoxelShape toNorth;
@@ -30,6 +37,7 @@ public class SpecialLanternBlock extends Block implements SimpleWaterloggedBlock
 
     public SpecialLanternBlock(Properties properties, VoxelShape shape) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, false));
         this.toEast = shape;
         this.toNorth = shape;
     }
@@ -57,6 +65,7 @@ public class SpecialLanternBlock extends Block implements SimpleWaterloggedBlock
     @Nullable
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
+        FluidState fluidState = context.getLevel().getFluidState(context.getClickedPos());
         BlockState blockstate = this.defaultBlockState();
         Level level = context.getLevel();
         BlockPos blockpos = context.getClickedPos();
@@ -67,7 +76,7 @@ public class SpecialLanternBlock extends Block implements SimpleWaterloggedBlock
                 Direction direction1 = direction.getOpposite();
                 blockstate = blockstate.setValue(FACING, direction1);
                 if (blockstate.canSurvive(level, blockpos)) {
-                    return blockstate;
+                    return blockstate.setValue(WATERLOGGED, fluidState.getType() == Fluids.WATER);
                 }
             }
         }
@@ -75,7 +84,26 @@ public class SpecialLanternBlock extends Block implements SimpleWaterloggedBlock
     }
 
     @Override
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos currentPos, BlockPos neighborPos) {
+        if (state.getValue(WATERLOGGED)) {
+            level.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+        }
+
+        return super.updateShape(state, direction, neighborState, level, currentPos, neighborPos);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    @Override
+    public boolean isPathfindable(BlockState state, BlockGetter level, BlockPos pos, PathComputationType type) {
+        return false;
+    }
+
+    @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(FACING, BlockStateProperties.WATERLOGGED);
+        builder.add(FACING, WATERLOGGED);
     }
 }
