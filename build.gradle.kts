7b6674f802d9ce0c4acc48fdf8668bbba74cfe9c
ThatGravyBoat <thatgravyboat@gmail.com>
2023-11-07 12:26:59 -0330

    Update embed

--- build.gradle.kts
+++ build.gradle.kts
@@ -154,12 +154,16 @@ resourcefulGradle {
             val minecraftVersion: String by project
             val version: String by project
             val changelog: String = file("changelog.md").readText(Charsets.UTF_8)
+            val fabricLink: String? = System.getenv("FABRIC_RELEASE_URL")
+            val forgeLink: String? = System.getenv("FORGE_RELEASE_URL")
 
             source.set(file("templates/embed.json.template"))
             injectedValues.set(mapOf(
                     "minecraft" to minecraftVersion,
                     "version" to version,
                     "changelog" to StringEscapeUtils.escapeJava(changelog),
+                    "fabric_link" to fabricLink,
+                    "forge_link" to forgeLink,
             ))
         }
     }
