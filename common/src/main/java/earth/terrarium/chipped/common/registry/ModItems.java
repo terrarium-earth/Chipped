df3f6ba01e079de18c43e2802e246e1a5286c64f
Alex Nijjar <alexander.nijjar@icloud.com>
2024-02-03 00:13:27 -0600

    Fix publishing

--- common/src/main/java/earth/terrarium/chipped/common/registry/ModItems.java
+++ common/src/main/java/earth/terrarium/chipped/common/registry/ModItems.java
@@ -1,6 +1,6 @@
 package earth.terrarium.chipped.common.registry;
 
-import com.teamresourceful.resourcefullib.common.item.tabs.ResourcefulCreativeModeTab;
+import com.teamresourceful.resourcefullib.common.item.tabs.ResourcefulCreativeTab;
 import com.teamresourceful.resourcefullib.common.registry.RegistryEntry;
 import com.teamresourceful.resourcefullib.common.registry.ResourcefulRegistries;
 import com.teamresourceful.resourcefullib.common.registry.ResourcefulRegistry;
@@ -8,18 +8,21 @@ import earth.terrarium.chipped.Chipped;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 import java.util.function.BiFunction;
+import java.util.function.Supplier;
 
 public class ModItems {
     public static final ResourcefulRegistry<Item> ITEMS = ResourcefulRegistries.create(BuiltInRegistries.ITEM, Chipped.MOD_ID);
     public static final ResourcefulRegistry<Item> BENCHES = ResourcefulRegistries.create(ITEMS);
 
-    public static final ResourcefulCreativeModeTab ITEM_GROUP = new ResourcefulCreativeModeTab(new ResourceLocation(Chipped.MOD_ID, "main"))
+    public static final Supplier<CreativeModeTab> ITEM_GROUP = new ResourcefulCreativeTab(new ResourceLocation(Chipped.MOD_ID, "main"))
         .setItemIcon(ModBlocks.BOTANIST_WORKBENCH)
-        .addRegistry(ITEMS);
+        .addRegistry(ITEMS)
+        .build();
 
     public static final RegistryEntry<Item> BOTANIST_WORKBENCH = BENCHES.register("botanist_workbench", () -> new BlockItem(ModBlocks.BOTANIST_WORKBENCH.get(), new Item.Properties()));
     public static final RegistryEntry<Item> GLASSBLOWER = BENCHES.register("glassblower", () -> new BlockItem(ModBlocks.GLASSBLOWER.get(), new Item.Properties()));
